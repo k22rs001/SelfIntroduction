c57c3d7acb4e9093fb2c9591aa2dfda279a14697
Aoyagi Takayuki <k22rs001@st.kyusan-u.ac.jp>
2024-04-25 17:32:00 +0900

    update

--- SelfIntroduction/SelfIntroduction/CounterViewController.swift
+++ SelfIntroduction/SelfIntroduction/CounterViewController.swift
@@ -9,5 +9,31 @@ import UIKit
 
 class CounterViewController: UIViewController {
     
-    @IBOutlet weak var count: UILabel!
+   
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var correctLabel: UILabel! {
+        didSet {
+            correctLabel.isHidden = true
+        }
+    }
+    var counterNumber = 0
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        counterNumber += 1
+        countLabel.text = "\(counterNumber)"
+        if counterNumber == 20 {
+            correctLabel.isHidden = false
+        } else {
+            correctLabel.isHidden = true
+        }
+    }
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        counterNumber -= 1
+        countLabel.text = "\(counterNumber)"
+        if counterNumber == 20 {
+            correctLabel.isHidden = false
+        } else {
+            correctLabel.isHidden = true
+        }
+    }
 }
