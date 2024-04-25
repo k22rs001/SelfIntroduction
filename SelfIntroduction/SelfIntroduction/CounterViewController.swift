e49f949bf7160b0011fc075401091f0e571121c8
Aoyagi Takayuki <k22rs001@st.kyusan-u.ac.jp>
2024-04-25 16:36:25 +0900

    update

---
+++ SelfIntroduction/SelfIntroduction/CounterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CounterViewController.swift
+//  SelfIntroduction
+//
+//  Created by Takayuki Aoyagi   on 2024/04/25.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+    
+    @IBOutlet weak var count: UILabel!
+}
--- SelfIntroduction/SelfIntroduction/ViewController.swift
+++ SelfIntroduction/SelfIntroduction/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        helloLabel.text = "こんにちは"
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
 
