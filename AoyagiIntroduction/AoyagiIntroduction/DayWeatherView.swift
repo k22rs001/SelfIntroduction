89417734a07c2a4209add6e8e41000a457f8523e
Aoyagi Takayuki <k22rs001@st.kyusan-u.ac.jp>
2024-05-13 09:25:49 +0900

    Update

--- AoyagiIntroduction/AoyagiIntroduction/DayWeatherView.swift
+++ AoyagiIntroduction/AoyagiIntroduction/DayWeatherView.swift
@@ -10,58 +10,66 @@ import WeatherKit
 import CoreLocation
 
 struct DayWeatherView: View {
-    @State var dayWeathers: [DayWeather] = []    // ----①
-    
+    @State var dayWeathers: [DayWeather] = []
+
     var body: some View {
-        ZStack{
+        ZStack {
             GeometryReader { geometry in
-                
                 Image("rain")
-                    .resizable()    // 画像サイズをフレームサイズに合わせる
-                    .frame(width: geometry.size.width, height: geometry.size.height)     // フレームサイズの指定
+                    .resizable()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                     .scaledToFill()
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                 VStack {
-                    VStack{
+                    VStack {
                         Text("TOKYO").font(.largeTitle)
                         Text("a")
                     }
-                    .frame(width: geometry.size.width * 0.9,alignment: .leading)
-                    .padding()
+                    .foregroundColor(.black)
+                    .frame(width: geometry.size.width * 0.8, alignment: .leading)
+                    .padding(.top, 70)
                     
-                    HStack{
-                        VStack{
-                            Text("3").font(.largeTitle)
+                    HStack {
+                        VStack {
+                            Text("3°").font(.largeTitle)
                             Text("a")
                         }
-                        .frame(width: geometry.size.width * 0.6, alignment: .leading)
-                        VStack{
+                        .foregroundStyle(.white)
+                        .frame(width: geometry.size.width * 0.5, alignment: .leading)
+                        VStack {
+                            Rectangle()
+                                .fill(Color.white)
+                                .frame(width: 1, height: 350)
+                                .multilineTextAlignment(.leading)
+                        }
+                        VStack {
                             Text("a")
                             Text("a")
                             Text("a")
                         }
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.leading,30)
                     }
                     ScrollView (.horizontal) {
-                        HStack{
-                            ForEach(dayWeathers, id: \.self.date) { weather in    // ----③
+                        HStack {
+                            ForEach(dayWeathers, id: \.self.date) { weather in
                                 VStack {
-                                    // 日付
-                                    Text(DateFormatter.localizedString(from: weather.date, dateStyle: .long, timeStyle: .none))
-                                        .foregroundStyle(.black)
-                                    Text(weather.condition.description)
-                                        .foregroundStyle(.black)
+                                    // 曜日と日にちのフォーマット
+                                    Text(formatDate(weather.date))
+                                    
                                     // 降水確率
                                     Text("\(Int((weather.precipitationChance*100)))%")
+                                        .padding()
                                     
                                     // 天気のシンボル
                                     Image(systemName: weather.symbolName)
                                 }
-                                .padding()
-                                .background()
+                                .foregroundColor(.white)
+                                .padding(.top, 150)
                             }
                         }
                         .task {
-                            // 日々の気象データを取得
                             await getWeather()
                         }
                     }
@@ -72,15 +80,23 @@ struct DayWeatherView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
+    
     func getWeather() async {
         let weatherService = WeatherService()
         let location = CLLocation(latitude: 35.6809591, longitude: 139.7673068) // 東京駅
         do {
-            let weather = try await weatherService.weather(for: location, including: .daily)    // ----②
+            let weather = try await weatherService.weather(for: location, including: .daily)
             dayWeathers = weather.forecast
         } catch {
             print(error)
         }
+    }
+    
+    // 日付を曜日と日にちのみにフォーマットする関数
+    private func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, MMM d" // 曜日と日にちのみにフォーマット
+        return dateFormatter.string(from: date)
     }
 }
 
